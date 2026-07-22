bf03a2ee6aa8daf2bf11850da65735886b04a420
ZainabM63 <zainabmughal33@gmail.com>
2026-07-23 01:53:11 +0500

    frontend fix

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // <-- Tells Next.js to build static HTML/CSS/JS into the /out folder
+  images: {
+    unoptimized: true, // <-- Required for static export
+  },
 };
 
 export default nextConfig;
