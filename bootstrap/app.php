615252215e186780c4d393920d672dc9dc8837ac
ZainabM63 <zainabmughal33@gmail.com>
2026-07-24 03:38:46 +0500

    fixed ver

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,8 +5,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 
-return Application::configure(basePath: dirname(__DIR__))
-    ->useStoragePath(env('APP_STORAGE', '/tmp/storage'))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
@@ -22,3 +21,8 @@ return Application::configure(basePath: dirname(__DIR__))
             fn (Request $request) => $request->is('api/*'),
         );
     })->create();
+
+// Set the storage path on the created Application instance
+$app->useStoragePath(env('APP_STORAGE', '/tmp/storage'));
+
+return $app;
